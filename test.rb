a7cecd9d5db182cdcf3ebb8eddbf12dbcd67f38e
bakins <brian.akins@turner.com>
2013-05-01 09:13:26 -0400

    Add stop and delete

--- test.rb
+++ test.rb
@@ -6,9 +6,9 @@ fifo = ProjectFifo.new("http://192.168.1.100/api/0.1.0/", "admin", "admin")
 
 fifo.connect
 
-fifo.vms.list.each do |vm|
-  pp fifo.vms[vm]
-end
+#fifo.vms.list.each do |vm|
+#  pp fifo.vms[vm]
+#end
 
 package = fifo.packages.list.first
 dataset = fifo.datasets.list.first
@@ -27,6 +27,16 @@ data = {
   }
 }
 
-pp data
 
-pp fifo.vms.create(data)
+vm = fifo.vms.create(data)
+
+pp vm
+
+id = vm['uuid']
+sleep 10
+
+fifo.vms.stop(id, true)
+
+sleep 10
+
+fifo.vms.delete(id)
